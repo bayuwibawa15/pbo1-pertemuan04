75544e394470c3da88be3a0bb776dd51db1135ef
bayuwibawa15 <wibawabayu015@gmail.com>
2020-04-22 12:21:20 +0700

    belajar Array 1

---
+++ src/Array/belajararray1.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class belajararray1 {
+    public static void main(String[] args) {
+        int nilaiArr[] = {40,55,16,17,19,10,20,15,12};
+
+        System.out.println(nilaiArr.length);
+        System.out.println(nilaiArr[0]);
+        System.out.println(nilaiArr[8]);
+    }
+}
