cf8d5eadb4ae4951af815debb0f0f202ac1edfd7
bayuwibawa15 <wibawabayu015@gmail.com>
2020-04-22 12:26:41 +0700

    belajar Array 2

---
+++ src/Array/belajararray2.java
@@ -0,0 +1,22 @@
+package Array;
+
+public class belajararray2 {
+    public static void main(String[] args) {
+        String mataKuliah[];
+        mataKuliah = new String[10];
+
+        mataKuliah[0] = "Pemrograman Berbasis Objek 1";
+        mataKuliah[1] = "Visual 2";
+        mataKuliah[2] = "Rekayasa Perangkat Lunak";
+        mataKuliah[3] = "Organisasi Komputer";
+        mataKuliah[4] = "Pemrograman Web 2";
+        mataKuliah[5] = "Pemrogaraman Berbasis Objek 2";
+        mataKuliah[6] = "Rekayasa Perangkat Lunak 2";
+        mataKuliah[7] = "Organisasi Komputer 2";
+        mataKuliah[8] = "Desain Web";
+        mataKuliah[9] = "Visual 3";
+
+        System.out.println("Matkul yang sedang kamu pelajari adalah : " + mataKuliah[2]);
+
+    }
+}
