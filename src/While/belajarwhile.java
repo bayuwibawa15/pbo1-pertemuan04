d5ff05c59def3a40a3f4f61ffdeedfffe18e2260
bayuwibawa15 <wibawabayu015@gmail.com>
2020-04-22 11:56:25 +0700

    belajar While

---
+++ src/While/belajarwhile.java
@@ -0,0 +1,18 @@
+package While;
+
+public class belajarwhile {
+    public static void main(String[] args) {
+        int isiGelas = 225;
+        int penuh = 225;
+
+        System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+        System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
+
+        while (isiGelas != penuh){
+            isiGelas++;
+            System.out.println("Sedang mengisi gelas...");
+            System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+        }
+        System.out.println("Finally: Isi Gelas Sekarang : " + isiGelas + "ml");
+    }
+}
